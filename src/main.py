e183987c21621c910ed3e2f2ad317d12aa96c4c6
Jarod Young <seizo@jarod-young.com>
2017-03-22 23:10:22 -0500

    add player as circle, initial movements

--- src/main.py
+++ src/main.py
@@ -6,8 +6,13 @@ import pygame
 pygame.init()
 
 #set size of screen
-GameDisplay = pygame.display.set_mode((800,600))
+SS = (800, 600)
+GameDisplay = pygame.display.set_mode(SS)
 running = True
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
 
 
 #set the title of the screen
@@ -15,6 +20,14 @@ pygame.display.set_caption('Vipy')
 
 clock = pygame.time.Clock()
 
+#the player, represented by a position for now
+player = [SS[0]//2, SS[1]//2]
+
+# radius of the player's circle
+player_size = 40
+
+#the speed the player can move
+player_velocity = 40
 
 while running:
     for event in pygame.event.get():
@@ -22,6 +35,20 @@ while running:
             running = False
             print("Exiting...")
 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            player[1] -= player_velocity
+        if keys[pygame.K_s]:
+            player[1] += player_velocity
+        if keys[pygame.K_a]:
+            player[0] -= player_velocity
+        if keys[pygame.K_d]:
+            player[0] += player_velocity
+
+        GameDisplay.fill(BLACK)
+        pygame.draw.circle(GameDisplay, RED, player, player_size, 0)
+
+        
         print(event)
 
         pygame.display.update()
