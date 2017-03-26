97017cf0bb52887c582f9a798efb1217672404e0
Jarod Young <seizo@jarod-young.com>
2017-03-26 14:47:26 -0500

    break game objects out into a module

--- src/main.py
+++ src/main.py
@@ -1,61 +1,13 @@
 #/bin/python3 
 
 import pygame
+from game_objects import *
 
 #initialize pygame
 pygame.init()
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-
 #set size of screen
 SS = (800, 600)
-
-
-class Entity(object):
-    def __init__(self, x, y, radius=10, color=RED):
-        self.x = x
-        self.y = y
-        self.color = color
-        self.__radius = radius
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, val):
-        self.__x = val
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, val):
-        self.__y = val
-
-    @property
-    def speed(self):
-        return 5
-
-    @property
-    def pos(self):
-        return (self.x, self.y)
-
-    @property
-    def color(self):
-        return self.__color
-
-    @color.setter
-    def color(self, val):
-        self.__color = val
-
-    @property
-    def radius(self):
-        return self.__radius
-
 
 GameDisplay = pygame.display.set_mode(SS)
 running = True
