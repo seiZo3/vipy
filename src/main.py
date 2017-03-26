cf42656c214947529b0bf4a6bed0b484f4c51fbe
Jarod Young <seizo@jarod-young.com>
2017-03-26 11:05:23 -0500

    move things around to account for other objects in the game

--- src/main.py
+++ src/main.py
@@ -13,11 +13,12 @@ BLACK = (0, 0, 0)
 SS = (800, 600)
 
 
-class Player():
-    def __init__(self, x, y, color = RED):
+class Entity(object):
+    def __init__(self, x, y, radius=10, color=RED):
         self.x = x
         self.y = y
         self.color = color
+        self.__radius = radius
 
     @property
     def x(self):
@@ -25,10 +26,7 @@ class Player():
 
     @x.setter
     def x(self, val):
-        if val < 0:
-            self.__x = 0
-        else:
-            self.__x = val
+        self.__x = val
 
     @property
     def y(self):
@@ -36,10 +34,15 @@ class Player():
 
     @y.setter
     def y(self, val):
-        if val < 0:
-            self.__y = 0
-        else:
-            self.__y = val
+        self.__y = val
+
+    @property
+    def speed(self):
+        return 5
+
+    @property
+    def pos(self):
+        return (self.x, self.y)
 
     @property
     def color(self):
@@ -51,22 +54,14 @@ class Player():
 
     @property
     def radius(self):
-        return 40
-
-    @property
-    def velocity(self):
-        return 5
-
-    @property
-    def pos(self):
-        return (self.x, self.y)
+        return self.__radius
 
 
 GameDisplay = pygame.display.set_mode(SS)
 running = True
 
 
-player = Player(SS[0]//2, SS[1]//2)
+player = Entity(SS[0]//2, SS[1]//2, radius=40, color=RED)
 
 #set the title of the screen
 pygame.display.set_caption('Vipy')
@@ -78,21 +73,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            print(event)
-            print("Exiting...")
     
 
     #get keys pressed
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_w] and player.y - player.radius > 0:
-        player.y -= player.velocity
+        player.y -= player.speed
     if keys[pygame.K_s] and player.y + player.radius < SS[1]:
-        player.y += player.velocity
+        player.y += player.speed
     if keys[pygame.K_a] and player.x - player.radius > 0:
-        player.x -= player.velocity
+        player.x -= player.speed
     if keys[pygame.K_d] and player.x + player.radius < SS[0]:
-        player.x += player.velocity
+        player.x += player.speed
+
+    #bullets
+    if keys[pygame.K_i]:
+        pass
 
     GameDisplay.fill(BLACK)
     pygame.draw.circle(GameDisplay, player.color, player.pos, player.radius, 0)
